e016b8c9fb0459743b7dc6e485dfc1aff99210b6
Kyle Harding <kylemharding@gmail.com>
2018-06-17 08:39:20 -0400

    refactoring

--- index.js
+++ index.js
@@ -13,103 +13,72 @@ config.endpoints = argv.endpoints;
 config.backupDir = argv.backupDir || "./" + argv.bridgeUser;
 
 /**
- * backup data to a local directory
+ * backup endpoint to a local directory
  * @param {string} backup directory
- * @param {array} hue endpoints
+ * @param {string} hue endpoint
  */
-module.exports.backup = (backupDir, endpoints) => {
+module.exports.backup = (backupDir, endpoint) => {
+
+    if (typeof endpoint === 'undefined') return;
     
-    const getEndpoint = (endpoint) => {
+    const outfile = backupDir + '/' + endpoint + '.json';
+    const url = 'http://' + config.bridgeIp + "/api/" + config.bridgeUser + "/" + endpoint;
+    
+    if (!fs.existsSync(backupDir)) {
+        console.log('creating ' + backupDir);
+        fs.mkdirSync(backupDir);
+    }
+    
+    console.log('getting ' + url);
+    request(url, { method: 'GET', json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
         
-        if (typeof endpoint === 'undefined') return;
-        
-        const outfile = backupDir + '/' + endpoint + '.json';
-        const url = 'http://' + config.bridgeIp + "/api/" + config.bridgeUser + "/" + endpoint;
-        
-        if (!fs.existsSync(backupDir)) {
-            console.log('creating ' + backupDir);
-            fs.mkdirSync(backupDir);
-        }
-        
-        console.log('getting ' + url);
-        request(url, { method: 'GET', json: true }, (err, res, body) => {
-            if (err) { return console.log(err); }
-            
-            fs.writeFile(outfile, JSON.stringify(body, null, 2), (err) => {  
-                if (err) throw err;
-                console.log('saved ' + outfile);
-            });
-            
-            return nextFn();
+        fs.writeFile(outfile, JSON.stringify(body, null, 2), (err) => {  
+            if (err) throw err;
+            console.log('saved ' + outfile);
         });
         
-        const nextFn = () => {
-            endpointIdx++;
-            if (endpointIdx >= endpoints.length) return;
-            
-            setTimeout( function(end) {
-                getEndpoint(end);
-            }.bind(this, endpoints[endpointIdx]), 1000);
-            return;
-        };
-    };
-    
-    let endpointIdx = 0;
-    getEndpoint(endpoints[endpointIdx]);
+        return nextEndpoint();
+    });
 };
 
-module.exports.restore = (backupDir, endpoints) => {
+/**
+ * restore endpoint from a local directory
+ * @param {string} backup directory
+ * @param {string} hue endpoint
+ */
+module.exports.restore = (backupDir, endpoint) => {
+        
+    const infile = backupDir + '/' + endpoint + '.json';
+    const url = 'http://' + config.bridgeIp + "/api/" + config.bridgeUser + "/" + endpoint;
     
-    const setEndpoint = (endpoint) => {
-        
-        const infile = backupDir + '/' + endpoint + '.json';
-        const url = 'http://' + config.bridgeIp + "/api/" + config.bridgeUser + "/" + endpoint;
-        
-        if (!fs.existsSync(infile)) {
-            console.log('backup file does not exist: ' + infile);
-            return;
-        }
-        
-        const resources = Object.values(JSON.parse(fs.readFileSync(infile, 'utf8')));
-        
-        const putResource = (resource) => {
+    const resources = Object.values(JSON.parse(fs.readFileSync(infile, 'utf8')));
+    
+    const putResource = (resource) => {
 
-            if (typeof resource === 'undefined') return nextFn();
-            
-            delete resource.owner;
-            delete resource.created;
-            delete resource.lasttriggered;
-            delete resource.timestriggered;
-            
-            console.log('putting ' + (resourceIdx + 1) + '/' + resources.length + ' ' + endpoint + ' to ' + url);
-            request(url, { method: 'PUT', json: true, body: resource}, (err, res, body) => {
-                if (err) { return console.log(err); }
-                console.log(body);
-                return nextFn();
-            });
-        };
+        if (typeof resource === 'undefined') return nextResource();
         
-        const nextFn = () => {
-            resourceIdx++;
-            if (resourceIdx >= resources.length) {
-                
-                endpointIdx++;
-                if (endpointIdx >= endpoints.length) return;
-                
-                setTimeout( function(end) {
-                    setEndpoint(end);
-                }.bind(this, endpoints[endpointIdx]), 0);
-                return;
-            }
-            
-            setTimeout( function(res) {
-                putResource(res);
-            }.bind(this, resources[resourceIdx]), 1000);
-            return;
-        };
+        delete resource.owner;
+        delete resource.created;
+        delete resource.lasttriggered;
+        delete resource.timestriggered;
         
-        let resourceIdx = 0;
-        putResource(resources[resourceIdx]);
+        console.log('putting ' + (resourceIdx + 1) + '/' + resources.length + ' ' + endpoint + ' to ' + url);
+        request(url, { method: 'PUT', json: true, body: resource}, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(body);
+            return nextResource();
+        });
+    };
+    
+    const nextResource = () => {
+        resourceIdx++;
+        if (resourceIdx >= resources.length) return nextEndpoint();
+        
+        setTimeout( function(res) {
+            putResource(res);
+        }.bind(this, resources[resourceIdx]), 1000);
+        return;
     };
     
     if (!fs.existsSync(backupDir)) {
@@ -117,8 +86,23 @@ module.exports.restore = (backupDir, endpoints) => {
         return;
     }
     
-    let endpointIdx = 0;
-    setEndpoint(endpoints[endpointIdx]);
+    if (!fs.existsSync(infile)) {
+        console.log('backup file does not exist: ' + infile);
+        return;
+    }
+    
+    let resourceIdx = 0;
+    putResource(resources[resourceIdx]);
 };
 
-this[argv._[0]](config.backupDir, config.endpoints);
+const nextEndpoint = () => {
+    endpointIdx++;
+    if (endpointIdx >= config.endpoints.length) return;
+    setTimeout( function(backupDir, endpoint) {
+        this[argv._[0]](backupDir, endpoint);
+    }.bind(this, config.backupDir, config.endpoints[endpointIdx]), 1000);
+    return;
+};
+
+let endpointIdx = 0;
+this[argv._[0]](config.backupDir, config.endpoints[endpointIdx]);
